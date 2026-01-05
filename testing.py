aff9ba126b4cdbcfffd8965ef0a42f1c0ebe25a1
root <root@SharmaBoss2022.localdomain>
2026-01-05 04:15:46 +0000

    this commit for actons leaning

--- testing.py
+++ testing.py
@@ -1,1 +1,8 @@
-print("hello dosto 2.0 let start learning for today and today only")
+def hello():
+    """
+    this is a hello world function
+    """ 
+    
+    print("hello dosto 2.0 let start learning for today and today only")
+
+hello()
